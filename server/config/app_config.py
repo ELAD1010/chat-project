69ca98fa3a2f29f88054f5dfd8e4452a0c05343f
ELAD1010 <elad2911@gmail.com>
2025-12-26 22:51:16 +0200

    add http_port to app_config

--- server/config/app_config.py
+++ server/config/app_config.py
@@ -7,3 +7,4 @@ class AppConfig:
         self.ip = os.getenv('IP')
         self.port = int(os.getenv('PORT'))
         self.db_name = os.getenv('DB_NAME')
+        self.http_port = int(os.getenv('HTTP_PORT'))
