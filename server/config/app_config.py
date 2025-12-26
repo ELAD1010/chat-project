4f76a2c648c6b5233d049a0b69e70c589b81fd36
ELAD1010 <elad2911@gmail.com>
2025-12-26 22:50:03 +0200

    add db_name to app_config

--- server/config/app_config.py
+++ server/config/app_config.py
@@ -1,6 +1,9 @@
 import os
+from server.decorators.singleton import singleton
 
+@singleton
 class AppConfig:
     def __init__(self):
         self.ip = os.getenv('IP')
         self.port = int(os.getenv('PORT'))
+        self.db_name = os.getenv('DB_NAME')
