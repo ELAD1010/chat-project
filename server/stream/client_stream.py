ac0c49aa3882450b32040d6fee7c11b625646467
ELAD1010 <elad2911@gmail.com>
2025-12-26 22:55:45 +0200

    add exception handling to socket unexpected disconnection or other errors

--- server/stream/client_stream.py
+++ server/stream/client_stream.py
@@ -2,7 +2,7 @@ import threading
 import socket
 import json
 from queue import Queue
-from chat.auxiliary.message import Message
+from auxiliary.message import Message
 from uuid import UUID
 
 BUFFER_SIZE = 1024
@@ -20,15 +20,43 @@ class ClientStream:
         threading.Thread(target=self.receive, daemon=True).start()
         threading.Thread(target=self.send, daemon=True).start()
 
+    def cleanup_connection(self):
+        self.cli_sock.shutdown(socket.SHUT_RDWR)
+        self.cli_sock.close()
+        for client_id in self.clients_dict:
+            if self.clients_dict == self.clients_dict[client_id]:
+                del self.clients_dict[client_id]
+
+        self.out_queue.put(None)  # Send signal to the send thread to be killed
+
     def receive(self):
-        data = self.cli_sock.recv(BUFFER_SIZE).decode()
-        message = Message.from_json(data)
-        dest_client = self.clients_dict[message.receiver_id]
-        dest_client.out_queue.put(message)
-        print(data)
+        while True:
+            try:
+                data = self.cli_sock.recv(BUFFER_SIZE)
+                if not data:
+                    print("Client disconnected cleanly")
+                    self.cleanup_connection()
+                    break
+
+                message = Message.from_json(data.decode())
+                dest_client = self.clients_dict[message.receiver_id]
+                dest_client.out_queue.put(message)
+
+            except ConnectionResetError:
+                # Handle abrupt os disconnection error
+                print("Client connection was forcibly closed")
+                self.cleanup_connection()
+                break
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                self.cleanup_connection()
+                break
 
     def send(self):
-        message: Message = self.out_queue.get()
-        data = json.dumps(message, default=lambda obj: obj.__dict__())
-        self.cli_sock.send(data.encode())
+        while True:
+            message: Message = self.out_queue.get()
+            if message is None:
+                break
+            data = json.dumps(message, default=lambda obj: obj.__dict__())
+            self.cli_sock.send(data.encode())
 
