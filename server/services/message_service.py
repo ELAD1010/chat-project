0a2832fdd933fa81fc75d4c9dd9b259ff68b3699
ELAD1010 <elad2911@gmail.com>
2026-01-08 00:33:49 +0200

    refactor message service to match the new db structure

--- server/services/message_service.py
+++ server/services/message_service.py
@@ -1,19 +1,24 @@
 from server.db.db_manager import DBManager
 from server.db.models import Message
 from auxiliary import message
-
+from server.utils import model_to_dict
 
 class MessageService:
     def __init__(self):
         self.db: DBManager = DBManager()
 
-    def get_messages_by_client_id(self, sender_id, receiver_id):
+    def get_messages_by_conversation_id(self, conversation_id):
         with self.db as session:
-            return session.query(Message)\
-                .filter((Message.sender_id == sender_id & Message.receiver_id == receiver_id)
-                        | (Message.sender_id == receiver_id & Message.receiver_id == sender_id)).all()
+            messages = session.query(Message)\
+                .filter((Message.conversation_id == conversation_id)).all()
+
+            messages_arr = [model_to_dict(m) for m in messages]
+            return messages_arr
 
     def create_message(self, message: message.Message):
-        db_message = Message(content=message.content, sender_id=message.sender_id, receiver_id=message.receiver_id)
         with self.db as session:
+            db_message = Message(content=message.content, sender_id=message.sender_id,
+                                 conversation_id="face2d8390a84b53a9bb1e87064eeaf8")
             session.add(db_message)
+            session.flush()
+            return db_message
